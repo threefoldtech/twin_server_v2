455671f232316a78aa7b8ec145c99a530d2877aa
ahmedhanafy725 <hanafya@codescalers.com>
2021-10-13 15:40:05 +0200

    Close connection for tfgrid after each call for modules

--- src/modules/contracts.ts
+++ src/modules/contracts.ts
@@ -1,44 +1,35 @@
-import { TFClient } from "grid3_client";
+import { TFGridClient } from "../clients/tfgrid";
 
 import { NodeContractCreate, NameContractCreate, ContractGet, NodeContractUpdate, ContractCancel } from "./models";
 import { expose } from "../helpers/expose";
-import { default as config } from "../../config.json";
 
 class Contracts {
-    client: TFClient;
+    client: TFGridClient;
+    context;
     constructor() {
-        this.client = new TFClient(config.url, config.mnemonic);
+        this.client = new TFGridClient();
+        this.context = this.client.contracts;
     }
+
     @expose
     async create_node(options: NodeContractCreate) {
-        await this.client.connect();
-        console.log(
-            `Creating contract with node_id:${options.node_id}, hash: ${options.hash}, data: ${options.data}, number of public ips: ${options.public_ip}`,
-        );
-        return await this.client.contracts.createNode(options.node_id, options.hash, options.data, options.public_ip);
+        return await this.client.execute(this.context, this.client.contracts.createNode, [options.node_id, options.hash, options.data, options.public_ip]);
     }
     @expose
     async create_name(options: NameContractCreate) {
-        await this.client.connect();
-        return await this.client.contracts.createName(options.name);
+        return await this.client.execute(this.context, this.client.contracts.createName, [options.name]);
     }
     @expose
     async get(options: ContractGet) {
-        await this.client.connect();
-        console.log(`Getting contract with id: ${options.id}`);
-        return await this.client.contracts.get(options.id);
+        return await this.client.execute(this.context, this.client.contracts.get, [options.id]);
     }
     @expose
     async update_node(options: NodeContractUpdate) {
-        await this.client.connect();
-        console.log(`Updating contract with id: ${options.id}, hash: ${options.hash}, data: ${options.data}`);
-        return await this.client.contracts.updateNode(options.id, options.data, options.hash);
+        return await this.client.execute(this.context, this.client.contracts.updateNode, [options.id, options.data, options.hash]);
     }
     @expose
     async cancel(options: ContractCancel) {
-        await this.client.connect();
-        console.log(`Cancel contract with id: ${options.id}`);
-        return await this.client.contracts.cancel(options.id);
+        return await this.client.execute(this.context, this.client.contracts.cancel, [options.id]);
     }
 }
 
